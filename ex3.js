d0c2f7dd1079b1bcd118e751eae27e1eed92fb1f
Jim Chien <jim_chien_br@hotmail.com>
2018-10-28 22:59:50 -0300

    Modificaçao ex3 e add ex4

--- ex3.js
+++ ex3.js
@@ -1,16 +1,27 @@
 function deepEqual(obj1,obj2) {
-    if (typeof(obj1) == typeof(ob2)) {
-        if (obj1 == obj2) {
-            return true;
+    compareObj1 = Object.values(obj1);
+    compareObj2 = Object.values(obj2);
+    for (let i = 0; i < compareObj1.length; i++) {
+        if (typeof(compareObj1[i]) == typeof(compareObj2[i])) {
+            if (compareObj1[i] == compareObj2[i]) {
+                console.log(true);
+            }
+            else {
+                console.log(false);
+            }
         }
-    }
-    else {
-        return false;
+        else {
+            console.log(false);
+        }
     }
 }
 
-let obj1 = {'nome': 'Pedro'};
-let obj2 = {'nome': 'Pedro'};
+let obj1 = {'nome': 'Pedro', 'numero': 2};
+let obj2 = {'nome': 'Pedro', 'numero': 3};
 
+//console.log(typeof(obj1));
+//console.log(typeof(obj2.nome));
+//console.log(typeof(obj1.nome));
+//console.log(Object.values(obj1));
 //console.log(obj1 === obj2);
-console.log(deepEqual(obj1,obj2)); 
+deepEqual(obj1,obj2);
